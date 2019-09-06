9f17f1404844e3c81edb8cc61fcd64e726f60320
Darby Dixon III <ddixon@oberlin.edu>
2019-09-06 11:29:43 -0400

    Initial commit.

---
+++ fractal.js
@@ -0,0 +1,15 @@
+'use strict';
+const path = require('path');
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/*
+ * Set the project title here.
+ */
+fractal.set('project.title', 'Fractal Starter Project');
+
+/*
+ * Tell Fractal where to look for components, docs, and static assets.
+ */
+fractal.components.set('path', path.join(__dirname, 'components'));
+fractal.docs.set('path', path.join(__dirname, 'docs'));
+fractal.web.set('static.path', path.join(__dirname, 'public'));
