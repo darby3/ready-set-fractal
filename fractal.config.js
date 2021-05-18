c7217dde224df14116f8f05ac5940ca38664e41d
Darby Dixon III <ddixon@oberlin.edu>
2021-05-18 11:10:02 -0400

    Updates.

--- fractal.config.js
+++ fractal.config.js
@@ -14,3 +14,21 @@ fractal.components.set('path', path.join(__dirname, 'components'));
 fractal.components.set('default.status', 'wip');
 fractal.docs.set('path', path.join(__dirname, 'docs'));
 fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+/**
+ * Static export destination.
+ */
+fractal.web.set('builder.dest', __dirname + '/build');
+
+/**
+ * Theme customization.
+ */
+const mandelbrot = require('@frctl/mandelbrot'); // require the Mandelbrot theme module
+
+// create a new instance with custom config options
+const myCustomisedTheme = mandelbrot({
+  skin: "teal"
+});
+
+// tell Fractal to use the configured theme by default
+fractal.web.theme(myCustomisedTheme);
